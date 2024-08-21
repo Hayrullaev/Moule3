0ea87b37242f33223977b99fce280dc27640c577
Shohruh <shoh0554770876@gmail.com>
2024-08-21 09:19:35 +0300

    Update Homework.py

--- Homework.py
+++ Homework.py
@@ -4,11 +4,12 @@ def count_calls():
     calls += 1
 
 def string_info(string):
-    upper_case = string.upper()
-    lower_case = string.lower()
-    return (len(string), upper_case, lower_case)
+     count_calls()
+    return len(string), string.upper, string.lower
+    
 
 def is_contains(string, list_to_search):
+    count_calls()
     for item in list_to_search:
         if item.lower().strip() == string.lower().strip():
             return True
